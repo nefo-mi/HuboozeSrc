1f26928134fb8cdabfaba0733f272210622670b9
nefo_mi <nefo.mi@gmail.com>
2012-06-14 01:00:25 +0900

    root変更

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/Hubooze/"
-http_images_path = "/Hubooze/images"
-http_fonts_path = "/Hubooze/fonts"
-css_dir = "public/Hubooze/stylesheets"
+http_path = "/"
+http_images_path = "/images"
+http_fonts_path = "/fonts"
+css_dir = "public/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
